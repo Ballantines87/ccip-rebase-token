8af9e664e028d420b36166e047fbb1052d5f9608
Ballantines87 <gm.soderbergh@gmail.com>
2025-10-01 17:35:55 +0200

    more, more and more comments

--- src/RebaseTokenPool.sol
+++ src/RebaseTokenPool.sol
@@ -26,16 +26,16 @@ contract RebaseTokenPool is TokenPool {
 
         // if we need to send some extra data to the destination chain, we can do it here -> and in the case of our rebase token, we need to be able to calculate the amount of tokens and how much interest they are earning -> so we need to calculate the user's interest rate -> and we can use that helpful getUserInterestRate() function that we created in our RebaseToken contract
 
-        address receiver = abi.decode(lockOrBurnIn.receiver, (address)); // we need to decode the receiver because it's in bytes format - which means that it has been previously encoded via abi.encode() into bytes -> so we call abi.decode() to get the original address back
+        address originalSender = lockOrBurnIn.originalSender; // we need to decode the receiver because it's in bytes format - which means that it has been previously encoded via abi.encode() into bytes -> so we call abi.decode() to get the original address back
 
         uint256 userInterestRate = IRebaseToken(address(i_token))
-            .getUserInterestRate(receiver);
+            .getUserInterestRate(originalSender);
 
         // now that we got the user's interest rate we can go ahead and burn our tokens
         // N.B. notice that we do .burn(address(this),...) -> because the way this CCIP works is 1) first the user does a token approval 2) then they send the tokens to CCIP 3) and then the CCIP will send to the Token Pool contract -> So actually it needs to be address(this) because the tokens are sent in the Token Pool contract when it is doing the cross-chain transfers -> which is why, when we do the cross-chain transfers - in our script and in our tests - we will need to approve the router for the amount of tokens that we wanna send cross-chain - hence the Token Pool contract itself (aka address(this)) is the one that needs to burn them
         IRebaseToken(address(i_token)).burn(address(this), lockOrBurnIn.amount);
 
-        // we finally need to return Pool.LockOrBurnOutV1 struct
+        // we finally need to return Pool.LockOrBurnOutV1 struct - because we're sending the message CROSS-CHAIN here
         lockOrBurnOut = Pool.LockOrBurnOutV1({
             // we just encode the REMOTE token address (NOT the local i_token) into bytes format -> to do that we use a helper function that's already implemented in the TokenPool contract called getRemoteToken()
             destTokenAddress: getRemoteToken(lockOrBurnIn.remoteChainSelector),
@@ -50,7 +50,7 @@ contract RebaseTokenPool is TokenPool {
         // this is called inside the releaseOrMint function -> because this is where the risk management network (RMN) checks happen and all validation occurs, to make sure our cross-chain mesages are secure -> so if this fails, the whole transaction reverts
         _validateReleaseOrMint(releaseOrMintIn);
 
-        // then we need to get the user's interest rate
+        // then we need to get the user's interest rate (that's the info that was sent cross-chain from the source chain to the destination chain)
         // this is in bytes format -> so we need to decode it
         uint256 userInterestRate = abi.decode(
             releaseOrMintIn.sourcePoolData,
@@ -58,7 +58,7 @@ contract RebaseTokenPool is TokenPool {
         );
 
         // then we need to set the user's interest rate -> BUT what happens if the user ALREADY has some tokens on this chain? -> we want to make sure that any interest is minted to them BEFORE we go ahead and set their interest rate again.
-
+        // and so we're mint(...)ing the tokens on this chain when we are receiving a cross-chain transfer to this chain to the receiver
         IRebaseToken(address(i_token)).mint(
             releaseOrMintIn.receiver,
             releaseOrMintIn.amount,
